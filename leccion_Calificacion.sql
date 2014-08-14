51433b0ce41b69acf1ed91a74b737dcb4bf1a64e
stefany <tefylove-jc@hotmail.com>
2014-08-14 11:34:31 -0500

    leccion

---
+++ leccion_Calificacion.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `leccion` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `leccion`;
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: leccion
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Calificacion`
+--
+
+DROP TABLE IF EXISTS `Calificacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Calificacion` (
+  `idCalificacion` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `nparcial` int(11) DEFAULT NULL,
+  `nfinal` int(11) DEFAULT NULL,
+  `nmejoramiento` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idCalificacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Calificacion`
+--
+
+LOCK TABLES `Calificacion` WRITE;
+/*!40000 ALTER TABLE `Calificacion` DISABLE KEYS */;
+INSERT INTO `Calificacion` VALUES (1,'stefy',60,70,80),(2,'maria',50,80,100),(3,'fabricio',80,60,90);
+/*!40000 ALTER TABLE `Calificacion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-14 11:33:15
